47761bc6fa8e5cf7a3698baf34772897398e355b
Issa Sangare <issabmsangare@hotmail.com>
2020-03-30 14:30:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,24 +1,28 @@
 def addition(num1, num2)
-sum = num1 + num2
+sum = num2 + num1
 sum
 end
 
 def subtraction(num1, num2)
-
+sub_traction = num2-num1
+sub_traction
 end
 
 def division(num1, num2)
-
+div_ision = num2 /num1
+div_ision
 end
 
 def multiplication(num1, num2)
-
+mul_tiplication = num1 * num2
+mul_tiplication
 end
 
 def modulo(num1, num2)
-
+mo_dulo = num2 % num1
 end
 
 def square_root(num)
-
+square_rt = num **2
+square_rt
 end
